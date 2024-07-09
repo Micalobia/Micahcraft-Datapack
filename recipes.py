a731af7aac5b5c8412d2c99a7fe6a20eea6c8760
Micah <micalobia2@gmail.com>
2024-07-09 12:36:05 -0700

    Woodcutting

--- recipes.py
+++ recipes.py
@@ -125,6 +125,14 @@ def advancement_path(*s):
     )
 
 
+def cutting_recipe(ingredient, result, count=1, is_item=False):
+    return {
+        "type": "stonecutting",
+        "ingredient": {"item" if is_item else "tag": ingredient},
+        "result": {"id": result, "count": count},
+    }
+
+
 print("Writing variant tags and stonecutting recipes...")
 for tag, values in tags.items():
     name = tag.split(":")[-1]
@@ -134,11 +142,11 @@ for tag, values in tags.items():
     for item in all_outputs[tag]:
         iname = item.split(":")[-1]
         with opend(recipe_path("variant", iname), "w") as f:
-            j = {
-                "type": "stonecutting",
-                "ingredient": {"tag": f"micahcraft:generated/variant/{name}"},
-                "result": {"id": item, "count": 2 if iname.endswith("_slab") else 1},
-            }
+            j = cutting_recipe(
+                f"micahcraft:generated/variant/{name}",
+                item,
+                2 if iname.endswith("_slab") else 1,
+            )
             json.dump(j, f, indent=4, default=sorted_list)
 
 
@@ -154,5 +162,108 @@ for path, recipe in recipes.items():
     p = f"./data{path.split('data')[-1]}"
     with opend(p, "w") as f:
         json.dump(empty, f)
+
+
+class LogType(BaseModel):
+    name: str
+    stem: bool = False
+
+    @property
+    def log(self):
+        return f"{self.name}_{'stem' if self.stem else 'log'}"
+
+    @property
+    def wood(self):
+        return f"{self.name}_{'hyphae' if self.stem else 'wood'}"
+
+    @property
+    def stripped_log(self):
+        return f"stripped_{self.name}_{'stem' if self.stem else 'log'}"
+
+    @property
+    def stripped_wood(self):
+        return f"stripped_{self.name}_{'hyphae' if self.stem else 'wood'}"
+
+    @property
+    def logs(self):
+        return f"{self.log}s"
+
+    @property
+    def woods(self):
+        return f"micahcraft:generated/wood/{self.name}"
+
+    @property
+    def boat(self):
+        if self.stem:
+            return "minecraft:air"
+        else:
+            return f"{self.name}_boat"
+
+    @property
+    def fence(self):
+        return f"{self.name}_fence"
+
+    @property
+    def gate(self):
+        return f"{self.name}_fence_gate"
+
+    @property
+    def planks(self):
+        return f"{self.name}_planks"
+
+    @property
+    def slab(self):
+        return f"{self.name}_slab"
+
+    @property
+    def stairs(self):
+        return f"{self.name}_stairs"
+
+    @property
+    def trapdoor(self):
+        return f"{self.name}_trapdoor"
+
+
+woods = [
+    LogType(name="acacia"),
+    LogType(name="birch"),
+    LogType(name="cherry"),
+    LogType(name="crimson", stem=True),
+    LogType(name="dark_oak"),
+    LogType(name="jungle"),
+    LogType(name="mangrove"),
+    LogType(name="oak"),
+    LogType(name="spruce"),
+    LogType(name="warped", stem=True),
+]
+
+print("Generating woodcutting tags...")
+for wood in woods:
+    tag = {"replace": False, "values": [wood.wood, wood.stripped_wood]}
+    with opend(tag_path("item", "wood", wood.name), "w") as f:
+        json.dump(tag, f, indent=4)
+
+
+print("Generating woodcutting recipes...")
+for wood in woods:
+    if not wood.stem:
+        with opend(recipe_path("woodcutting", wood.name, "boat_from_wood"), "w") as f:
+            json.dump(cutting_recipe(wood.woods, wood.boat), f, indent=4)
+    r = {
+        "fence_from_wood": cutting_recipe(wood.woods, wood.fence, 3),
+        "fence_gate_from_wood": cutting_recipe(wood.woods, wood.gate),
+        "planks_from_log": cutting_recipe(wood.logs, wood.planks, 4),
+        "slabs_from_log": cutting_recipe(wood.logs, wood.slab, 8),
+        "slabs_from_planks": cutting_recipe(wood.planks, wood.slab, 2, True),
+        "stairs_from_log": cutting_recipe(wood.logs, wood.stairs, 4),
+        "stairs_from_planks": cutting_recipe(wood.planks, wood.stairs, 1, True),
+        "stripped_log": cutting_recipe(wood.log, wood.stripped_log, 1, True),
+        "stripped_wood": cutting_recipe(wood.wood, wood.stripped_wood, 1, True),
+        "trapdoor_from_log": cutting_recipe(wood.logs, wood.trapdoor),
+    }
+    for k,v in r.items():
+        with opend(recipe_path("woodcutting", wood.name, k), "w") as f:
+            json.dump(v, f, indent=4)
+    
 
 print("Finished")
